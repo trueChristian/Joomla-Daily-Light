1b9906b9ca49d4e287065b6cf379a1d363e1947c
aB0t <robot@vdm.io>
2023-07-26 11:21:40 +0200

    Release of v1.1.3

--- README.md
+++ README.md
@@ -1,17 +1,19 @@
-# Dailylight (1.1.2)
+# Dailylight (1.1.3)
 
 Display the daily light from [https://git.vdm.dev/christian/daily-light](https://git.vdm.dev/christian/daily-light).
 
+> Support can be found at our [community support area](https://git.vdm.dev/getBible/support).
+
 # Build Details
 
-+ *Company*: [Vast Development Method](https://www.vdm.io/)
++ *Company*: [Vast Development Method](https://getbible.net)
 + *Author*: [Llewellyn van der Merwe](mailto:joomla@vdm.io)
-+ *Name*: [Dailylight](https://www.vdm.io/)
-+ *First Build*: 22nd October, 2015
-+ *Last Build*: 29th May, 2023
-+ *Version*: 1.1.2
++ *Name*: [Dailylight](https://getbible.net)
++ *First Build*: 3rd December, 2015
++ *Last Build*: 26th July, 2023
++ *Version*: 1.1.3
 + *Copyright*: Copyright (C) 2015. All Rights Reserved
 + *License*: GNU/GPL Version 2 or later - http://www.gnu.org/licenses/gpl-2.0.html
 
 > This **module** was build with a Joomla [Automated Component Builder](https://www.joomlacomponentbuilder.com).
-> Developed by [Llewellyn van der Merwe](mailto:llewellyn@joomlacomponentbuilder.com)
+> Developed by [Llewellyn van der Merwe](mailto:joomla@vdm.io)
--- script.php
+++ script.php
@@ -1,26 +1,19 @@
 <?php
-/*-------------------------------------------------------------------------------------------------------------|  www.vdm.io  |------/
- ____                                                  ____                 __               __               __
-/\  _`\                                               /\  _`\   __         /\ \__         __/\ \             /\ \__
-\ \,\L\_\     __   _ __    ___ ___     ___     ___    \ \ \/\ \/\_\    ____\ \ ,_\  _ __ /\_\ \ \____  __  __\ \ ,_\   ___   _ __
- \/_\__ \   /'__`\/\`'__\/' __` __`\  / __`\ /' _ `\   \ \ \ \ \/\ \  /',__\\ \ \/ /\`'__\/\ \ \ '__`\/\ \/\ \\ \ \/  / __`\/\`'__\
-   /\ \L\ \/\  __/\ \ \/ /\ \/\ \/\ \/\ \L\ \/\ \/\ \   \ \ \_\ \ \ \/\__, `\\ \ \_\ \ \/ \ \ \ \ \L\ \ \ \_\ \\ \ \_/\ \L\ \ \ \/
-   \ `\____\ \____\\ \_\ \ \_\ \_\ \_\ \____/\ \_\ \_\   \ \____/\ \_\/\____/ \ \__\\ \_\  \ \_\ \_,__/\ \____/ \ \__\ \____/\ \_\
-    \/_____/\/____/ \/_/  \/_/\/_/\/_/\/___/  \/_/\/_/    \/___/  \/_/\/___/   \/__/ \/_/   \/_/\/___/  \/___/   \/__/\/___/  \/_/
+/*----------------------------------------------------------------------------------|  io.vdm.dev  |----/
+			Vast Development Method
+/-------------------------------------------------------------------------------------------------------/
 
-/------------------------------------------------------------------------------------------------------------------------------------/
+    @package    getBible.net
 
-	@version		2.1.x
-	@created		22nd October, 2015
-	@package		Sermon Distributor
-	@subpackage		script.php
-	@author			Llewellyn van der Merwe <https://www.vdm.io/>	
-	@copyright		Copyright (C) 2015. All Rights Reserved
-	@license		GNU/GPL Version 2 or later - http://www.gnu.org/licenses/gpl-2.0.html 
+    @created    3rd December, 2015
+    @author     Llewellyn van der Merwe <https://getbible.net>
+    @git        Get Bible <https://git.vdm.dev/getBible>
+    @github     Get Bible <https://github.com/getBible>
+    @support    Get Bible <https://git.vdm.dev/getBible/support>
+    @copyright  Copyright (C) 2015. All Rights Reserved
+    @license    GNU/GPL Version 2 or later - http://www.gnu.org/licenses/gpl-2.0.html
 
-	A sermon distributor that links to Dropbox. 
-
-/----------------------------------------------------------------------------------------------------------------------------------*/
+/------------------------------------------------------------------------------------------------------*/
 
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
@@ -32,33 +25,4 @@ defined('_JEXEC') or die('Restricted access');
  */
 class mod_DailyLightInstallerScript
 {
-
-	/**
-	 * Called before any type of action
-	 *
-	 * @param   string  $route  Which action is happening (install|uninstall|discover_install|update)
-	 * @param   Joomla\CMS\Installer\InstallerAdapter  $adapter  The object responsible for running this script
-	 *
-	 * @return  boolean  True on success
-	 */
-	public function preflight($route, $adapter)
-	{
-		// get application
-		$app = JFactory::getApplication();
-
-		// the default for both install and update
-		$jversion = new JVersion();
-		if (!$jversion->isCompatible('3.8.0'))
-		{
-			$app->enqueueMessage('Please upgrade to at least Joomla! 3.8.0 before continuing!', 'error');
-			return false;
-		}
-
-		if ('install' === $route)
-		{
-//
-		}
-
-		return true;
-	}
 }
